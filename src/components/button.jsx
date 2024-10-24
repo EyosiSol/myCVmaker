98efd941940737533c31f5fbbe39abecfb4c76fd
Eyosi Sol <eyosila123@gmail.com>
2024-10-24 21:22:51 +0300

    make tweaks on the button component and download button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-function Button({ svg, name, classname }) {
+function Button({ svg, name, classname,}) {
   const Styles = {
     display: "flex",
     alignItems: "center",
@@ -10,12 +10,15 @@ function Button({ svg, name, classname }) {
     border: 'none',
     borderRadius: 12+'px',
     height: 60+'px',
+    fontSize : 1.2 +'rem',
+    fontWeight : '600'
   };
+
   return (
     <>
       <button className={classname} style={Styles}>
         <img src={svg} alt={name} />
-        <h4>{name}</h4>
+        {name}
       </button>
     </>
   );
@@ -24,5 +27,10 @@ Button.propTypes = {
   name: PropTypes.string,
   svg: PropTypes.string,
   classname: PropTypes.string,
+  widz: PropTypes.string,
+  heit: PropTypes.string,
+  fontsize: PropTypes.string,
+  fontWeit: PropTypes.string
+
 };
 export default Button;
